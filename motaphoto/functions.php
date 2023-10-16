d2f96232bba02752b9225af61324ecab967d2950
Al <alicemilkovich63@gmail.com>
2023-10-16 14:20:29 +0200

    Intégration header et footer desktop + ajout des images

--- motaphoto/functions.php
+++ motaphoto/functions.php
@@ -1,7 +1,18 @@
 <?php
 
+//Chargement des scripts CSS et JavaScript
+
 function motaphoto_enqueue_styles()
 {
     wp_enqueue_style('style', get_stylesheet_uri(), array());
 }
 add_action('wp_enqueue_scripts', 'motaphoto_enqueue_styles');
+
+//Ajout des menus pour le header et le footer
+
+function motaphoto_register_menu()
+{
+    register_nav_menu('header-menu', __('Menu header', 'motaphoto'));
+    register_nav_menu('footer-menu', __('Menu footer', 'motaphoto'));
+}
+add_action('after_setup_theme', 'motaphoto_register_menu');
--- motaphoto/header.php
+++ motaphoto/header.php
@@ -9,3 +9,13 @@
 </head>
 
 <body>
+    <nav role="navigation" class="nav_header" aria-label="<?php _e('Menu header', 'motaphoto'); ?>">
+        <img class="logo" alt="Nathalie Mota" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>">
+        <?php
+        wp_nav_menu([
+            'theme_location' => 'header-menu',
+            'container' => false,
+            'menu_class' => 'menu header'
+        ])
+        ?>
+    </nav>
